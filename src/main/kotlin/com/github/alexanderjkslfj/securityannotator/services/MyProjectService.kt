187fb0af4dc640d7cb7efe9caab260ff4ac6b72e
GitHub Action <action@github.com>
2025-11-06 16:46:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/alexanderjkslfj/securityannotator/services/MyProjectService.kt
+++ src/main/kotlin/com/github/alexanderjkslfj/securityannotator/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.alexanderjkslfj.securityannotator.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.alexanderjkslfj.securityannotator.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
