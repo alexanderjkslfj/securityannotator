04fd07ce78dd4ad693d0025509d2a712c5fc78a8
Alex <alexander.cinnamon927@passmail.net>
2026-01-22 00:36:46 +0100

    integrate parser

--- src/main/java/com/github/alexanderjkslfj/securityannotator/toolWindow/LLMWindow.java
+++ src/main/java/com/github/alexanderjkslfj/securityannotator/toolWindow/LLMWindow.java
@@ -6,6 +6,8 @@ import com.github.alexanderjkslfj.securityannotator.dataPackage.PromptBuilder;
 import com.github.alexanderjkslfj.securityannotator.annotator.CreateSampleAnnotation;
 import com.github.alexanderjkslfj.securityannotator.annotator.Annotator;
 
+import com.github.alexanderjkslfj.securityannotator.util.Annotation;
+import com.github.alexanderjkslfj.securityannotator.util.Parser;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.options.ShowSettingsUtil;
 import com.intellij.openapi.project.Project;
@@ -21,6 +23,7 @@ import java.awt.*;
 import java.io.IOException;
 
 public class LLMWindow implements ToolWindowFactory{
+    private java.util.List<Annotation> toBeApplied = null;
 
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
@@ -30,6 +33,8 @@ public class LLMWindow implements ToolWindowFactory{
         JLabel label = new JLabel("LLM Analyzer", SwingConstants.CENTER);
 
         JButton analyzeButton = new JButton("Run Analysis");
+        JButton applyButton = new JButton("Apply Result");
+        applyButton.setEnabled(false);
         JButton apiKeySetterButton = new JButton("Enter API Key");
         JButton annotationGenerator = new JButton("generate dummy annotations");
 
@@ -39,6 +44,7 @@ public class LLMWindow implements ToolWindowFactory{
 
         JPanel buttonPanel = new JPanel(new FlowLayout());
         buttonPanel.add(analyzeButton);
+        buttonPanel.add(applyButton);
         buttonPanel.add(apiKeySetterButton);
         buttonPanel.add(annotationGenerator);
 
@@ -50,15 +56,26 @@ public class LLMWindow implements ToolWindowFactory{
             ShowSettingsUtil.getInstance().showSettingsDialog(project, Settings.class);
         });
 
+        applyButton.addActionListener(e -> {
+            if(toBeApplied != null) {
+                Annotator.insertFeatureComment(project, toBeApplied);
+                toBeApplied = null;
+                applyButton.setEnabled(false);
+                outputArea.setText("Annotations applied.");
+            }
+        });
+
         analyzeButton.addActionListener(e -> {
-            String promptInput = null;
+            applyButton.setEnabled(false);
+            analyzeButton.setEnabled(false);
+            String promptInput;
             try {
                 promptInput = PromptBuilder.buildPrompt(project);
             } catch (IOException ex) {
                 throw new RuntimeException(ex);
             }
-            outputArea.setText(promptInput);
-            //outputArea.setText(" Sending to LLM...");
+
+            outputArea.setText("Analyzing code...");
 
             PromptService service = ApplicationManager
                     .getApplication()
@@ -66,15 +83,38 @@ public class LLMWindow implements ToolWindowFactory{
 
             // Send the content to the LLM
             service.prompt(promptInput)
-                    .thenAccept(response -> ApplicationManager.getApplication().invokeLater(() -> {
-                        outputArea.setText("LLM Response:" + response);
-                    }))
-                    .exceptionally(ex -> {
-                        ApplicationManager.getApplication().invokeLater(() -> {
-                            outputArea.setText("Error: " + ex.getMessage());
-                        });
-                        return null;
+                .thenAccept(response -> ApplicationManager.getApplication().invokeLater(() -> {
+                    analyzeButton.setEnabled(true);
+                    if(response != null) {
+                        java.util.List<Annotation> list = Parser.parse(project, response);
+                        if(list != null && !list.isEmpty()) {
+                            toBeApplied = list;
+
+                            String text = "Annotations found:\n";
+                            for (Annotation a : list) {
+                                if(a.start_line() == a.end_line()) {
+                                    text += "Line " + a.start_line() + ": " + a.category() + "\n";
+                                } else {
+                                    text += "Lines " + a.start_line() + " to " + a.end_line() + ": " + a.category() + "\n";
+                                }
+                            }
+                            outputArea.setText(text);
+
+                            applyButton.setEnabled(true);
+                        } else {
+                            outputArea.setText("No annotations found.");
+                        }
+                    } else {
+                        outputArea.setText("No annotations found.");
+                    }
+                }))
+                .exceptionally(ex -> {
+                    ApplicationManager.getApplication().invokeLater(() -> {
+                        analyzeButton.setEnabled(true);
+                        outputArea.setText("Error: " + ex.getMessage());
                     });
+                    return null;
+                });
         });
 
         //currently generates hand made annotations, should later use LLM response data
